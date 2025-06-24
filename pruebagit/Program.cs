1f9f953c8637fa60acbef78fa49fa6ddf66100b8
“bojorquezja” <bojorquez.ja@gmail.com>
2025-06-24 17:30:17 -0500

    mejoras xyz

--- pruebagit/Program.cs
+++ pruebagit/Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hola soy gianma");
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
